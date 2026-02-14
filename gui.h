91c8974c2c8560aee57a492c4cbf6285bfb5f263
Chamuditha Madhushan <chamudithamadushan54@gmail.com>
2026-02-14 22:50:19 +0530

    Add gui.h header file with game print functions

---
+++ gui.h
@@ -0,0 +1,13 @@
+// gui.h
+#ifndef GUI_H
+#define GUI_H
+
+#include <stdio.h>
+
+void printGameTitle(void);
+void printFirstFloor(void);
+void printSecondFloor(void);
+void printThirdFloor(void);
+void printLegend(void);
+
+#endif
